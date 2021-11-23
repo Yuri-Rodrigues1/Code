7619f5835760067d1366aa20d5c3043c99b599e6
Yuri Rodrigues Martins <wyurirodrigues@gmail.com>
2021-11-23 20:54:39 -0300

    Add files via upload

---
+++ Livro.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author Yure
+ */
+public class Livro {
+    private String tituloLivro;
+    private double autorLivro;
+
+    public String getTituloLivro() {
+        return tituloLivro;
+    }
+
+    public void setTituloLivro(String tituloLivro) {
+        this.tituloLivro = tituloLivro;
+    }
+
+    public String getAutorLivro() {
+        return autorLivro;
+    }
+
+    public void setAutorLivro(String autorLivro) {
+        this.autorLivro = autorLivro;
+    }
+
+    
+    
+}
